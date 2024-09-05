6fa687c7c2dcb5851572a708777114ae0a106bce
RATHEESH G KUMAR <152181631+ratheeshkumar25@users.noreply.github.com>
2024-09-05 20:31:27 +0530

    Time: 102 ms (58.25%), Space: 7.5 MB (96.23%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,4 +1,37 @@
 func findKthLargest(nums []int, k int) int {
-    sort.Ints(nums)
-    return nums[len(nums)-k]
+     heapSort(nums)
+     return nums[len(nums)-k]
+}
+
+func heapify(arr[]int,idx int, n int){
+     larg := idx
+     left := 2*idx+1
+     right := 2*idx+2
+
+     if left<n && arr[left]>arr[larg]{
+        larg = left
+     }
+     if right<n && arr[right]>arr[larg]{
+        larg = right
+     }
+
+     if larg != idx{
+        arr[idx],arr[larg] = arr[larg],arr[idx]
+        heapify(arr,larg,n)
+     }
+}
+
+func buildHeap(arr[]int,n int){
+    for i:= n/2-1; i>=0 ; i--{
+        heapify(arr,i,n)
+    }
+}
+
+func heapSort(arr[]int){
+     n := len(arr)
+     buildHeap(arr,n)
+     for i:=n-1; i>0;i--{
+        arr[0],arr[i]= arr[i],arr[0]
+        heapify(arr,0,i)
+     }
 }
