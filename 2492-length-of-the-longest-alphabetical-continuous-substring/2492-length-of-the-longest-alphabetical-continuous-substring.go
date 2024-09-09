5a484896f7ee7a65dcfa9bb62f62a6c59daa140d
RATHEESH G KUMAR <152181631+ratheeshkumar25@users.noreply.github.com>
2024-09-09 21:40:49 +0530

    Time: 13 ms (55.32%), Space: 6.6 MB (53.19%) - LeetHub

--- 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.go
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.go
@@ -1,17 +1,15 @@
 func longestContinuousSubstring(s string) int {
     res := 0
-    curr :=1
-    for i :=1; i<len(s);i++{
-         //fmt.Println("----",string(s[i]),string(s[i-1]))
-        if s[i-1]+1== s[i]{
+    curr := 1
+
+    for i:=1;i<len(s);i++{
+        if s[i-1]+1 == s[i]{
             curr++
-           // fmt.Println("////",curr)
         }else{
-            res = max(curr,res)
-            //fmt.Println("*****",curr,res)
+            res = max(res,curr)
             curr = 1
         }
     }
-    res = max(curr,res)
-    return res
+    res = max(res,curr)
+    return res 
 }
