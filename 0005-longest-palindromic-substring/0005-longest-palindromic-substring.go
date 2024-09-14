ccbac6f568e1e4d543cdc472a5324fa20384af7e
RATHEESH G KUMAR <152181631+ratheeshkumar25@users.noreply.github.com>
2024-09-14 08:06:19 +0530

    Time: 6 ms (57%), Space: 2.6 MB (100%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -1,51 +1,29 @@
 func longestPalindrome(s string) string {
-	// res := ""
-	// max := 0
+	res:= "" 
+    max := 0
 
-	// for i := range s {
-	// 	l, r := i, i
-	// 	for l >= 0 && r < len(s) && s[l] == s[r] {
-	// 		if max <= (r - l) {
-	// 			max = r - l
-	// 			res = s[l : r+1]
-	// 		}
-	// 		l--
-	// 		r++
-	// 	}
-	// 	l, r = i, i+1
-	// 	for i >= 0 && r < len(s) && s[l] == s[r] {
-	// 		if max < (r - l) {
-	// 			max = r - l
-	// 			res = s[l : r+1]
-	// 		}
-	// 		l--
-	// 		r++
+	for i := range s {
+		l, r := i, i
+		for l >= 0 && r < len(s) && s[l] == s[r] {
+			if max <= (r - l) {
+				max = r - l
+				res = s[l : r+1]
+			}
+			l--
+			r++
+		}
+		l, r = i, i+1
+		for l >= 0 && r < len(s) && s[l] == s[r] {
+			if max < (r - l) {
+				max = r - l
+				res = s[l : r+1]
+			}
+			l--
+			r++
 
-	// 	}
+		}
 
-	// }
-	// return res
+	}
+	return res
 
-        res,max := "",0
-    for i := range s {
-        l,r := i,i
-        for l >= 0 && r < len(s) && s[l] == s[r] {
-            if max <= (r-l) {
-                max = r-l
-                res = s[l:r+1]
-            }
-            l--
-            r++
-        }
-        l,r = i,i+1
-        for l >= 0 && r < len(s) && s[l] == s[r] {
-            if max < (r-l) {
-                max = r-l
-                res = s[l:r+1]
-            }
-            l--
-            r++
-        }
-    }
-    return res
 }
